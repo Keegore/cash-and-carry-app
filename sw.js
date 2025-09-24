d286b41486cd36515fb1237b15c036af445b5bf0
Keegore <hkeeganb@gmail.com>
2025-09-24 17:59:25 +0000

    Added PWA

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'cash-and-carry-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
